8dd7cb419789d71107c2f9fb24e16e948d2f8f31
Kevin Kim <kevin.kim1@qcells.com>
2026-05-14 13:52:32 -0400

    Fix GitHub Pages router base path

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ import SavedPlaces from "./components/SavedPlaces";
 
 export default function App() {
     return (
-        <BrowserRouter>
+        <BrowserRouter basename={process.env.PUBLIC_URL || "/"}>
             <Routes>
                 <Route path="/" element={<Layout />}>
                     <Route index element={<Home />} />
